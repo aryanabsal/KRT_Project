02a5a8af5db9705d63c2b77804ad8b97cc865bad
Aryan Absalan <aryanabsalan@Aryans-MacBook-Pro.local>
2024-05-31 18:06:06 +0330

    README.md Updated

---
+++ Kristall-token/hardhat.config.js
@@ -0,0 +1,7 @@
+require('@babel/register');
+require('hardhat/config');
+require ("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig */
+export const solidity = "0.8.17";
+
